71b25a5370b7085c35f9eeffcb003d60dea462c7
Zerto233 <180951159+Zerto233@users.noreply.github.com>
2026-06-23 18:25:30 +0800

    更新CORS 配置

--- src/main/java/com/zerto/myblog/config/CorsConfig.java
+++ src/main/java/com/zerto/myblog/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://my-blog.pages.dev",
+                        "https://my-blog-bbr.pages.dev",
                         "https://my-blog-backend-production-dbbd.up.railway.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
